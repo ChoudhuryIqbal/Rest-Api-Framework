29df1e9f4372ff827d4429510be8bdc5721aa84a
iqbal <iqbalchoudhury2@gmail.com>
2017-03-29 11:00:52 -0400

    data Provider added

--- src/test/java/com/iqbal/testScripts/TC_001.java
+++ src/test/java/com/iqbal/testScripts/TC_001.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.google.gson.Gson;
@@ -16,19 +17,26 @@ import com.jayway.restassured.response.Response;
 
 public class TC_001 {
 	Response response;
-	ArrayList<String > brandName;
+	ArrayList<Object> brandName;
+	ArrayList<Object> bagId;
+	ArrayList<Object> laptopName;
+	// ArrayList<Object> bagId;
+
 	@BeforeClass
 	public void setUp() {
-		brandName=new ArrayList<String>();
-		brandName.add("Dell");
-		brandName.add("Apple");
+		brandName = new ArrayList<Object>();
+		// brandName.add("Dell");
+		// brandName.add("Apple");
+		// id=new ArrayList<Object>();
+		laptopName = new ArrayList<Object>();
+		bagId = new ArrayList<Object>();
 
 	}
 
-	@Test(enabled=false)
+	@Test(enabled = false)
 	public void verifyGetMessage() {
 
-		String url = URL.lapTopBagUrl+EndPointUrl.PING_MESSAGE.getResourcePath("pingman");
+		String url = URL.lapTopBagUrl + EndPointUrl.PING_MESSAGE.getResourcePath("pingman");
 		response = Webservices.Get(url);
 		System.out.println(url);
 		System.out.println(response.getBody().asString());
@@ -41,34 +49,66 @@ public class TC_001 {
 		GetBag getBag[];
 
 		String url = URL.lapTopBagUrl + EndPointUrl.GET_BAG.getResourcePath();
-		 //System.out.println(url);
+		// System.out.println(url);
 		response = Webservices.Get(url);
-		
+
 		System.out.println(response.getBody().asString());
 
 		if (response.getStatusCode() == 200) {
 			getBag = gson.fromJson(response.getBody().asString(), GetBag[].class);
-			//String getComp[]={"Dell","Apple"};
+			// String getComp[]={"Dell","Apple"};
 			for (int i = 0; i < getBag.length; i++) {
-				//System.out.println(getBag[0].getBrandName());
-				Assert.assertEquals(brandName.get(i), getBag[i].getBrandName());
-			
-			
+				// System.out.println(getBag[0].getBrandName());
+				Assert.assertEquals(new Integer(i + 126), getBag[i].getId());
+				brandName.add(getBag[i].getBrandName());
+				laptopName.add(getBag[i].getLaptopName());
+				bagId.add(getBag[i].getId());
+
 			}
 
 		}
 
 	}
-
+@Test(dataProvider="getBagById")
+	public void verifyGetById(Integer id,String brand,String lname) {
+		
+	Gson gson=new Gson();
+	GetBag getBag;
 	
-	public void verifyGetById() {
-		String url = URL.lapTopBagUrl.concat(EndPointUrl.GET_BAG_BY_ID.getResourcePath("126"));
-
+	
+	
+	String url = URL.lapTopBagUrl.concat(EndPointUrl.GET_BAG_BY_ID.getResourcePath(id.toString()));
+		
 		response = Webservices.Get(url);
-		System.out.println(response.getBody().asString());
+		//System.out.println(response.getBody().asString());
+		
+		if (response.getStatusCode()==200){
+			getBag=gson.fromJson(response.getBody().asString(), GetBag.class);
+			//Assert.assertEquals(new Integer(i + 126), getBag[i].getId());
+			//brandName.add(brand,getBag.getBrandName());
+			//laptopName.add(getBag[i].getLaptopName());
+			//bagId.add(getBag[i].getId());
+			Assert.assertEquals(brand,getBag.getBrandName());
+			Assert.assertEquals(lname, getBag.getLaptopName());
+			Assert.assertEquals(id, getBag.getId().toString());
+		}
 	}
 
-	
+	@DataProvider(name = "getBagById")
+	public Object[][] getBagById() {
+
+		Object[][] result = new Object[2][1];
+		for (int i = 0; i < result.length; i++) {
+			result[i][0] = bagId.get(i);
+			result[i][1]=brandName.get(i);
+			result[i][2]=laptopName.get(i);
+			
+
+		}
+		return result;
+
+	}
+
 	public void VerifyAddCountry() {
 
 	}
